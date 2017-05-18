19bae91de2522696c3f7659cd51954d26303b77a
naumenko-sa <sergey_bulk@list.ru>
2017-05-18 17:19:21 -0400

    modified:   vcf.samtools.get_depth.sh

--- vcf.samtools.get_depth.sh
+++ vcf.samtools.get_depth.sh
@@ -2,5 +2,8 @@
 
 bname=`echo $1 | sed s/.vcf.gz//`
 
+#DP for sample is alt depth 
+#-GF AD is useless- not set
+
 gatk -R ~/work/tools/bcbio/genomes/Hsapiens/GRCh37/seq/GRCh37.fa -T VariantsToTable -V $1 \
      -F CHROM -F POS -F REF -F ALT  -F DP -GF DP -o $bname.table --allowMissingData 
