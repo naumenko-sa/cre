c3b2ae9d790dbbe8402cdbe4b467b062ad90d431
naumenko-sa <sergey_bulk@list.ru>
2017-07-11 00:00:49 -0400

    modified:   bcl2fastq.sh

--- bcl2fastq.sh
+++ bcl2fastq.sh
@@ -3,7 +3,7 @@
 #PBS -l walltime=23:00:00,nodes=1:ppn=10
 #PBS -joe .
 #PBS -d .
-#PBS -l vmem=20g,mem=20g
+#PBS -l vmem=30g,mem=30g
 
 if [ -z $sample_sheet ];
 then
@@ -11,6 +11,8 @@ then
 fi
 
 #2.19 needs 20G of RAM for 10 threads
+# sometimes 20G is not enough - I've got an error
+# bcl2fastq is designed up to 32G
 module load bcl2fastq
 
 #I use no-lane splitting, and the result fastq files could not be uploaded to basespace
