2a8b7dfb2ab015332559b5e7481432456814ae77
naumenko-sa <sergey_bulk@list.ru>
2017-07-14 12:37:44 -0400

    modified:   cre.locate_sample.py 	deleted:    bam.validate.sh 	deleted:    fixit.sh 	deleted:    gemini.vep2gemini.sh

--- cre.locate_sample.py
+++ cre.locate_sample.py
@@ -17,27 +17,27 @@ match = re.match('\d*',family)
 
 if match:
     prefix=str(int(match.group(0))/100)
-    report_path = prefix+'x/'+family+'/'+family
+    report_path = prefix+'x/'+family
     
     report=0
     bam=0
     
     errors = []
     
-    if os.path.isfile(report_path+'.csv'):
+    if os.path.isfile(report_path+'/'+family+'.csv'):
 	#print("Report exists")
 	report=1
     else:
 	errors.append('Error: no report')
 	
-    if os.path.isfile(report_path+'_'+sample_only+'.bam'):
+    if os.path.isfile(report_path+'/'+sample+'.bam'):
 	#print("Bam exists")
 	bam=1
     else:
 	errors.append(' ERROR: no bam')
 	
     if (bam==1 and report==1):
-        print(sample+'\t'+os.getcwd()+"/"+family+"\t"+os.getcwd()+"/"+report_path+'.bam')
+        print(sample+'\t'+os.getcwd()+"/"+report_path+"\t"+os.getcwd()+"/"+report_path+'/'+sample+'.bam')
     else:
 	print(sample+'\t'+' '.join(errors))
 else:
