8ae6ef619aa0828afe473708320f378c77ce2068
naumenko-sa <evolgenomicslab@gmail.com>
2017-11-06 09:41:05 -0500

    modified:   cre.database.pull_gene.sh

--- cre.database.pull_gene.sh
+++ cre.database.pull_gene.sh
@@ -5,7 +5,7 @@ database_prefix=$2
 prefix=`date +%Y-%m-%d`
 
 head -n1 $database_prefix.c4r.variant_wise.csv > $prefix.$gene.variant_wise.csv
-cat $database_prefix.c4r.variant_wise.csv | grep $gene >> $prefix.$gene.variant_wise.csv
+cat $database_prefix.c4r.variant_wise.csv | grep ","$gene"," >> $prefix.$gene.variant_wise.csv
 
 cat ~/cre/cre.database.header1 > $prefix.$gene.sample_wise.csv
-cat $database_prefix.c4r.sample_wise.csv | grep $gene | awk -F "," '{print $NF","$0}' | sort >> $prefix.$gene.sample_wise.csv
+cat $database_prefix.c4r.sample_wise.csv | grep ","$gene"," | awk -F "," '{print $NF","$0}' | sort >> $prefix.$gene.sample_wise.csv
