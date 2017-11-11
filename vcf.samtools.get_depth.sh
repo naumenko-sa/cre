1e7f8f0863242296c0978f9f8b6ff92ea849f564
naumenko-sa <evolgenomicslab@gmail.com>
2017-11-11 17:48:25 -0500

    modified:   vcf.freebayes.getAO.sh 	modified:   vcf.gatk.get_depth.sh 	modified:   vcf.platypus.getNV.sh 	modified:   vcf.samtools.get_depth.sh

--- vcf.samtools.get_depth.sh
+++ vcf.samtools.get_depth.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-bname=`echo $1 | sed s/.vcf.gz//`
+bname=`basename $1 .vcf.gz`
 
 #DP for sample is alt depth 
 #-GF AD is useless- not set
 
-gatk -R ~/work/tools/bcbio/genomes/Hsapiens/GRCh37/seq/GRCh37.fa -T VariantsToTable -V $1 \
+if [ -e $1 ]
+then
+    gatk -R ~/work/tools/bcbio/genomes/Hsapiens/GRCh37/seq/GRCh37.fa -T VariantsToTable -V $1 \
      -F CHROM -F POS -F REF -F ALT  -F DP -GF DP -o $bname.table 
      #--allowMissingData 
+fi
