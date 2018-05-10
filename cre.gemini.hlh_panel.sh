8710d822cfb27d821f103cb5ee7955ae267e5749
naumenko-sa <evolgenomicslab@gmail.com>
2018-05-10 11:22:29 -0400

    modified:   cre.gemini.hlh_panel.sh

--- cre.gemini.hlh_panel.sh
+++ cre.gemini.hlh_panel.sh
@@ -2,7 +2,7 @@
 
 # HLH gene panel for MAS diagnostics
 
-gemini query --header -q "select chrom,end,ref,alt,gene,rs_ids,impact,max_aaf_all 
-			  from variants 
-			  where max_aaf_all <= 0.05 and 
-			  gene in ('AP3B1','BLOC1S6','CD27','GATA2','ITK','LYST','NLRC4','PRF1','RAB27A','SH2D1A','SLC7A7','STX11','STXBP2','UNC13D','XIAP')" $1
+gemini query --header -q "select s.name as sample,v.chrom,v.start+1 as pos,v.ref,v.alt,v.gene,v.rs_ids,v.impact,v.max_aaf_all 
+			  from variants v, samples s
+			  where v.max_aaf_all <= 0.05 and 
+			  v.gene in ('AP3B1','BLOC1S6','CD27','GATA2','ITK','LYST','NLRC4','PRF1','RAB27A','SH2D1A','SLC7A7','STX11','STXBP2','UNC13D','XIAP')" $1
