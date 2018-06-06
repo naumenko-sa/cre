d96d54d947de34efb9a1ab376a044b9889dd461b
naumenko-sa <evolgenomicslab@gmail.com>
2018-06-06 09:20:09 -0400

    modified:   cre.sh

--- cre.sh
+++ cre.sh
@@ -13,7 +13,7 @@
 # 	type = [ wes.regular (default) | wes.synonymous | wes.fast | rnaseq | wgs ]
 ####################################################################################################
 
-#PBS -l walltime=10:00:00,nodes=1:ppn=1
+#PBS -l walltime=20:00:00,nodes=1:ppn=1
 #PBS -joe .
 #PBS -d .
 #PBS -l vmem=20g,mem=20g
