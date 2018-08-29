d557e38d1e0ddd3f1181b6f651c6b7ed731a4a7f
naumenko-sa <evolgenomicslab@gmail.com>
2018-08-29 16:06:36 -0400

    modified:   cre.bcbio.custom_genome.sh 	modified:   cre.bcbio.fresh_install.sh 	modified:   cre.bcbio.templates.wes.noalign.yaml

--- cre.bcbio.custom_genome.sh
+++ cre.bcbio.custom_genome.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-bcbio_setup_genome.py -f $1 -g /hpf/largeprojects/ccmbio/naumenko/tools/bcbio/genomes/Hsapiens/GRCh37/rnaseq/ref-transcripts.gtf -n Hsapiens -b GRCh37d5 -i bwa star rtg
+# crashes on qlogin node
+
+#PBS -l walltime=48:00:00,nodes=1:ppn=40
+#PBS -joe .
+#PBS -d .
+#PBS -l vmem=20g,mem=20g
+
+hostname
+echo $PATH
+echo $PYTHONPATH
+
+bcbio_setup_genome.py -f hs37d5.fa -g /hpf/largeprojects/ccmbio/naumenko/tools/bcbio/genomes/Hsapiens/GRCh37/rnaseq/ref-transcripts.gtf -n Hsapiens -b GRCh37d5 -i bwa star rtg -c 40
 
