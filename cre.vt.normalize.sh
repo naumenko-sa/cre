8328f1ce0952a9717a86ce8de3172edef96f501e
naumenko-sa <evolgenomicslab@gmail.com>
2018-10-24 14:28:44 -0400

    modified:   cre.vt.normalize.sh

--- cre.vt.normalize.sh
+++ cre.vt.normalize.sh
@@ -5,7 +5,9 @@
 #PBS -d .
 #PBS -l vmem=20g,mem=20g
 
+# do not fail if a variant in inconsistent with the reference
+
 bname=`basename $vcf .vcf.gz`
 
-vt normalize -r ~/work/tools/bcbio/genomes/Hsapiens/GRCh37/seq/GRCh37.fa $vcf | vt uniq - -o $bname.normalized.vcf.gz
+vt normalize -r ~/work/tools/bcbio/genomes/Hsapiens/GRCh37/seq/GRCh37.fa $vcf -n | vt uniq - -o $bname.normalized.vcf.gz
 tabix $bname.normalized.vcf.gz
