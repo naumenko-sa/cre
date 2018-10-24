86ac869e560449874fec9b7da9b2f1d50ea09bd6
naumenko-sa <evolgenomicslab@gmail.com>
2018-10-24 14:46:25 -0400

    modified:   cre.vt.decompose.sh

--- cre.vt.decompose.sh
+++ cre.vt.decompose.sh
@@ -11,6 +11,6 @@ bname=`basename $1 .vcf.gz`
 #already fixed in the new version
 #cat $bname.vcf | sed s/"##FORMAT=<ID=AD,Number=."/"##FORMAT=<ID=AD,Number=R"/ > $bname.modified.vcf
 
-vt decompose -s $1 | vt normalize -r ~/work/tools/bcbio/genomes/Hsapiens/GRCh37/seq/GRCh37.fa - | vt uniq - -o $bname.decomposed.vcf.gz
+vt decompose -s $1 | vt normalize -r ~/work/tools/bcbio/genomes/Hsapiens/GRCh37/seq/GRCh37.fa -n - | vt uniq - -o $bname.decomposed.vcf.gz
 
 tabix $bname.decomposed.vcf.gz
