1d7a064a1f530632e8fdf1abdbdc9163f8f9c8c0
naumenko-sa <evolgenomicslab@gmail.com>
2018-12-13 15:30:45 -0500

    modified:   cre.sh 	modified:   vcf.platypus.getNV.gatk3.sh

--- cre.sh
+++ cre.sh
@@ -210,7 +210,8 @@ function f_make_report
 	bcftools view -R ${family}-ensemble.db.txt.positions $fprefix.vcf.gz | bcftools sort | vt decompose -s - | vt uniq - -o $fprefix.subset.vcf.gz
 	tabix $fprefix.subset.vcf.gz
 	f_fix_sample_names $fprefix
-	vcf.platypus.getNV.gatk3.sh $fprefix.subset.vcf.gz $reference
+	# for some reason there was vcf.platypus.getNV.gatk3.sh - memory bug
+	vcf.platypus.getNV.sh $fprefix.subset.vcf.gz $reference
     fi
 
     cd ..
