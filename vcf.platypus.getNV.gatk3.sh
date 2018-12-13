e9f7cd5491da49294f65ac78e73dadafd32dfc44
naumenko-sa <evolgenomicslab@gmail.com>
2018-12-13 15:36:46 -0500

    modified:   cre.sh 	modified:   vcf.platypus.getNV.gatk3.sh

--- vcf.platypus.getNV.gatk3.sh
+++ vcf.platypus.getNV.gatk3.sh
@@ -6,7 +6,7 @@ bname=`basename $1 .subset.vcf.gz`
 
 if [ -e $1 ]
 then
-    gatk3 -Xmx10G -T VariantsToTable \
+    gatk3 -T VariantsToTable \
     -R $2 \
     -V $1 \
     -F CHROM -F POS -F REF -F ALT -F TC -GF NR -GF NV \
