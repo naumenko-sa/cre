8a67ddfcedd4a56e09eb320bff0894311b2fe712
naumenko-sa <evolgenomicslab@gmail.com>
2018-12-14 15:00:03 -0500

    modified:   vcf.split_multi.sh

--- vcf.split_multi.sh
+++ vcf.split_multi.sh
@@ -2,7 +2,8 @@
 
 # $1 = family.vcf.gz
 
-for sample in `cat samples.txt`; 
+for sample in `bcftools queru -l $1`
 do 
-    bcftools view -c1 -Ov -s $sample -o $sample.vcf $1;
-done;
+    bcftools view -c1 -Ov -s $sample -o $sample.vcf $1
+done
+
