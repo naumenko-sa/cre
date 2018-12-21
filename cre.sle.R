493b8ed7e9b18135f8564b239a70a9c6f739af29
naumenko.sa <evolgenomicslab@gmail.com>
2018-12-21 16:53:45 -0500

    modified:   cre.sle.R

--- cre.sle.R
+++ cre.sle.R
@@ -16,12 +16,18 @@ primary_immunodeficiency = read.csv("~/cre/data/primary_immunodeficiency.csv", s
 
 periodic_fever = read.csv("~/cre/data/periodic_fever_syndromes.csv", stringsAsFactors = F)
 
+mas = read.csv("~/cre/data/mas.csv")
+
+recurrent_fever_SK = read.csv("~/cre/data/recurrent_fever_syndrome_panel_from_crm.csv")
+
 variants$Lupus_panel = ifelse(variants$Ensembl_gene_id %in% lupus$ensembl_gene_id,"Lupus_panel",NA)
 variants$Lupus_langefeld_gwas = ifelse(variants$Ensembl_gene_id %in% lupus.langefeld.gwas$ensembl_gene_id,"Lupus_langefeld_gwas",NA)
 variants$Lupus_eastasianssle_gwas = ifelse(variants$Ensembl_gene_id %in% lupus.eastasianssle.gwas$ensembl_gene_id,"Lupus_eastasianssle_gwas",NA)
 variants$Primary_immunodeficiency_panel = ifelse(variants$Ensembl_gene_id %in% primary_immunodeficiency$PanelAPP.EnsemblId.GRch37,"Primary_immunodeficiency_panel",NA)
 variants$Periodic_fever_panel = ifelse(variants$Ensembl_gene_id %in% periodic_fever$PanelAPP.EnsemblId.GRch37,"Periodic_fever",NA)
+variants$MAS = ifelse(variants$Ensembl_gene_id %in% mas$ensembl_gene_id,"MAS",NA)
+variants$Recurrent_fever_SK = ifelse(variants$Ensembl_gene_id %in% recurrent_fever_SK$ensembl_gene_id, "Recurrent_fever_SK",NA)
 
-output_report = sub(".csv",".lupus.csv",input_report)
+output_report = sub(".csv",".immunopanels.csv",input_report)
 
 write.csv(variants,output_report,row.names = F)
