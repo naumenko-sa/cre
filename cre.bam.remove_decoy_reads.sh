a6b7bdead56e9896500515e07a0ae42b880f82ad
naumenko-sa <evolgenomicslab@gmail.com>
2019-01-11 00:09:26 -0500

    modified:   cre.bam.remove_decoy_reads.sh 	new file:   data/grch37d5.decoy.bed

--- cre.bam.remove_decoy_reads.sh
+++ cre.bam.remove_decoy_reads.sh
@@ -15,8 +15,5 @@ VARIANT_BAM_PATH=/hpf/largeprojects/ccmbio/naumenko/tools/VariantBam
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$XZ_LIB
 
 bname=`basename $bam .bam`
-#removing reads with grep is necessary for manta: https://github.com/walaj/VariantBam/issues/16
-$VARIANT_BAM_PATH/variant $bam -L /hpf/largeprojects/ccmbio/naumenko/tools/bcbio/genomes/Hsapiens/GRCh37d5/seq/decoy.bed -v | grep -v hs37d5 | grep -v NC_007605 | samtools view - -hb > $bname.no_decoy_reads.bam
-
-
-
+#removing reads with grep is necessary for manta: https://github.com/walaj/VariantBam/issues/16, probably no need in VariantBam here
+$VARIANT_BAM_PATH/variant $bam -L ~/cre/data/grch37d5.decoy.bed -v | grep -v hs37d5 | grep -v NC_007605 | samtools view - -hb > $bname.no_decoy_reads.bam
