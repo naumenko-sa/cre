fd089b225d91f1d08ec66727f0e4ed0c915feff3
naumenko-sa <evolgenomicslab@gmail.com>
2019-01-11 00:05:26 -0500

    modified:   cre.bam.remove_decoy_reads.sh

--- cre.bam.remove_decoy_reads.sh
+++ cre.bam.remove_decoy_reads.sh
@@ -4,7 +4,19 @@
 #PBS -d .
 #PBS -l vmem=10g,mem=10g
 
-module load gcc/5.2.0
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/hpf/largeprojects/ccmbio/naumenko/tools/xz-install/lib/
+#variant bam: https://github.com/walaj/VariantBam
+#needs to install xz and newer gcc
 
-/hpf/largeprojects/ccmbio/naumenko/tools/VariantBam/variant 159_CH0315-ready.bam -L /hpf/largeprojects/ccmbio/naumenko/tools/bcbio/genomes/Hsapiens/GRCh37d5/seq/decoy.bed -v | grep -v hs37d5 | grep -v NC_007605 | samtools view - -hb > 159_CH0315.bam
+module load gcc/5.2.0
+
+XZ_LIB=/hpf/largeprojects/ccmbio/naumenko/tools/xz-install/lib/
+VARIANT_BAM_PATH=/hpf/largeprojects/ccmbio/naumenko/tools/VariantBam
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$XZ_LIB
+
+bname=`basename $bam .bam`
+#removing reads with grep is necessary for manta: https://github.com/walaj/VariantBam/issues/16
+$VARIANT_BAM_PATH/variant $bam -L /hpf/largeprojects/ccmbio/naumenko/tools/bcbio/genomes/Hsapiens/GRCh37d5/seq/decoy.bed -v | grep -v hs37d5 | grep -v NC_007605 | samtools view - -hb > $bname.no_decoy_reads.bam
+
+
+
