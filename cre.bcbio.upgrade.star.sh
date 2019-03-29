a73b5f263309d5d8e13a4374d47251db8166c36b
naumenko-sa <evolgenomicslab@gmail.com>
2019-03-29 06:13:56 -0400

    modified:   cre.bcbio.upgrade.star.sh

--- cre.bcbio.upgrade.star.sh
+++ cre.bcbio.upgrade.star.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l walltime=240:00:00,nodes=1:ppn=10
+#PBS -l walltime=240:00:00,nodes=1:ppn=30
 #PBS -joe .
 #PBS -d .
 #PBS -l vmem=31g,mem=31g
@@ -14,4 +14,4 @@ hostname
 export PATH=/hpf/largeprojects/ccmbio/naumenko/tools/bcbio_testing/bin:$PATH && STAR \
 --genomeDir /hpf/largeprojects/ccmbio/naumenko/tools/bcbio_testing/genomes/Hsapiens/GRCh37/star \
 --genomeFastaFiles /hpf/largeprojects/ccmbio/naumenko/tools/bcbio_testing/genomes/Hsapiens/GRCh37/seq/GRCh37.fa \
---runThreadN 10 --limitGenomeGenerateRAM 30000000000 --genomeChrBinNbits 14 --runMode genomeGenerate --genomeSAindexNbases 14
+--runThreadN 30 --limitGenomeGenerateRAM 30000000000 --genomeChrBinNbits 14 --runMode genomeGenerate --genomeSAindexNbases 14
