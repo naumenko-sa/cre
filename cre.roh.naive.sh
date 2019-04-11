41ca93cc410803e22a4f447e4840d3c9bc72fcea
naumenko-sa <evolgenomicslab@gmail.com>
2019-04-11 12:02:03 -0400

    ROH naive update

--- cre.roh.naive.sh
+++ cre.roh.naive.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 # runs of HOM MAF 5% variant in proband (CH0620) affected sib (CH0621) and in both
-# gt_types=0 or 3 = HOM, 1 = HET
+# filters DP=50, QUAL>=500
+# gt_types=0=HOM_REF or 3 = HOM_ALT, 1 = HET
 # usage:
 # cre.roh.naive.sh sample gemini.db [maf=0.05]
+# output:
+# - sample.roh_variants.tsv - list of variants, print with tee for debugging
+# - stdout: list of ROH
 
 sample=$1
-echo "chrom,pos,ref,alt,impact,qual,dp,gene,maf,gts."$sample",gt_types."$sample",stretch_length_variants,stretch_length_bp,stretch_id,stretch_genes"
+echo "chrom,pos,ref,alt,impact,qual,dp,gene,maf,gts."$sample",gt_types."$sample",gt_alt_depths."$sample",stretch_length_variants,stretch_length_bp,stretch_id,stretch_genes" | tee $sample.roh_variants.tsv
 
-maf=0.05
-if [ -n $3 ]
+maf=1
+if [ -n "$3" ]
 then
     maf=$3
 fi
 
-gemini query -q "select chrom,start+1 as pos, ref, alt,impact,qual,dp, gene, gnomad_af_popmax as maf, gts."$sample",gt_types."$sample" from variants where
-type='snp' and dp>20 and qual>100 and gnomad_af_popmax<"$maf --gt-filter "gt_types."$sample" != 2" $2 | sort -k1,1n -k2,2n \
-| awk -F "\t" '
+gemini query -q "select chrom,start+1 as pos, ref, alt,impact,qual,dp, gene, gnomad_af_popmax as maf, gts."$sample",gt_types."$sample",gt_alt_depths."$sample" from variants where
+type='snp' and dp>=50 and qual>=500 and gnomad_af_popmax<="$maf --gt-filter "gt_types."$sample" != 2" $2 | sed s/"\t"/","/g | sort -t "," -k1,1n -k2,2n \
+| tee -a $sample.roh_variants.tsv | awk -F "," '
 BEGIN{
     prev=1;
     prev_gene="";
@@ -55,9 +59,9 @@ BEGIN{
     }
     prev=genotype;
     prev_chrom=$1;
-    print $0"\t"stretch_length_variants"\t"stretch_length_bp"\t"stretch_id"\t\""stretch_genes"\"";
-}' | sed s/"\t"/","/g | grep -v "0$" | awk -F ',' '{ if ($12>9) print $0;}' \
-| sort -k1,1 -k14,14n \
+    print $0","stretch_length_variants","stretch_length_bp","stretch_id",\""stretch_genes"\"";
+}' | grep -v "0$" | awk -F ',' '{ if ($13>9) print $0;}' \
+| sort -t "," -k1,1 -k15,15n \
 | awk -F "," '
 BEGIN{
     prev_chr="";
@@ -68,12 +72,12 @@ BEGIN{
     if($1 != prev_chr){
 	print prev_stretch;
     }else{
-	if (prev_stretch_id != $14){
+	if (prev_stretch_id != $15){
 	    print prev_stretch;
 	}
     }
     prev_stretch=$0;
     prev_chr=$1;
-    prev_stretch_id=$14;
+    prev_stretch_id=$15;
 }' | grep -v "^$"
 
