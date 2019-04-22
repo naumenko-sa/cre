fa63e450c9dacddf4013028b20d971163935ab4c
naumenko-sa <evolgenomicslab@gmail.com>
2019-04-22 15:30:50 -0400

    modified rohet_naive analysis

--- cre.rohet.naive.sh
+++ cre.rohet.naive.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
-# runs of HETEROZYGOUS VARIANTS MAF 5% variant in proband (CH0620) affected sib (CH0621) and in both
-# filters for WGS DP=50, QUAL>=100
+# runs of HETEROZYGOUS VARIANTS MAF 5% 
+# filters for WGS DP=10 (dp is low for WGS), QUAL>=500
 # gt_types 1 = HET
+
 # usage:
 # cre.roh.naive.sh sample gemini.db [maf=0.05]
 # output:
 # - sample.roh_variants.tsv - list of variants, print with tee for debugging
-# - stdout: list of ROH
+# - stdout: list of ROHET
 
 sample=$1
-echo "chrom,pos,ref,alt,impact,qual,dp,gene,maf,gts."$sample",gt_types."$sample",gt_alt_depths."$sample",stretch_length_variants,stretch_length_bp,stretch_id,stretch_genes" | tee $sample.roh_variants.tsv
+echo "chrom,stretch_end,ref,alt,impact,qual,dp,gene,maf,gts."$sample",gt_types."$sample",gt_alt_depths."$sample",stretch_length_variants,stretch_length_bp,stretch_start,stretch_genes" | tee $sample.rohet_variants.csv | \
+awk -F ',' '{print $1","$15","$2","$13","$14","$16}'
 
 maf=0.05
 if [ -n "$3" ]
 then
     maf=$3
 fi
-#depth for 3 samples
+#depth is for 3 samples
 gemini query -q "select chrom,start+1 as pos, ref, alt,impact,qual,depth, gene, max_aaf_all as maf, gts."$sample",gt_types."$sample",gt_alt_depths."$sample" from variants where 
-type='snp' and depth>=30 and qual>=100 and max_aaf_all<="$maf --gt-filter "gt_types."$sample" != 2" $2 | sed s/"\t"/","/g | sort -t "," -k1,1n -k2,2n \
+type='snp' and depth>=10 and qual>=500 and max_aaf_all<="$maf --gt-filter "gt_types."$sample" != 2" $2 | grep -v chrGL | sed s/chr// | sed s/"\t"/","/g | sort -t "," -k1,1 -k2,2n \
 | tee -a $sample.rohet_variants.csv | awk -F "," '
 BEGIN{
     prev_genotype=0;
@@ -42,7 +44,7 @@ BEGIN{
 	    stretch_length_variants=stretch_length_variants+1;
 	    stretch_length_bp=$2-stretch_id+1;
 	    if ($8 != prev_gene && $8 != ""){
-    		stretch_genes=stretch_genes","$8;
+    		stretch_genes=stretch_genes";"$8;
     	    }
     	    prev_gene=$8;
 	}else{
@@ -75,5 +77,5 @@ BEGIN{
     prev_stretch=$0;
     prev_chr=$1;
     prev_stretch_id=$15;
-}' | grep -v "^$"
+}' | grep -v "^$" | awk -F ',' '{print $1","$15","$2","$13","$14","$16}'
 
