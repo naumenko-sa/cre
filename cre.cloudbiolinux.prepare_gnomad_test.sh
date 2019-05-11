aa7ed1d8975aad0bd85c18c66db712d6dafb680f
naumenko-sa <evolgenomicslab@gmail.com>
2019-05-11 01:25:01 -0400

    gnomad_test

--- cre.cloudbiolinux.prepare_gnomad_test.sh
+++ cre.cloudbiolinux.prepare_gnomad_test.sh
@@ -2,11 +2,13 @@
 # prepares full chomosome test set for gnomad to test ggd recipe
 mkdir txtmp
 cd txtmp
-prefix=gnomad.exomes.r2.1.sites.grch38.chr
+type="genomes"
+prefix=gnomad.$type.r2.1.sites.chr
+# Y is absent for gnomad genomes
 for chrom in $(seq 1 22;echo X Y)
 do
-  curl -r 0-1000000 -O http://ftp.ensemblorg.ebi.ac.uk/pub/data_files/homo_sapiens/GRCh38/variation_genotype/gnomad/r2.1/exomes/${prefix}${chrom}_noVEP.vcf.gz
-  gunzip -c ${prefix}${chrom}_noVEP.vcf.gz | head -n 1400 > ${prefix}${chrom}_noVEP.vcf
+  curl -r 0-1000000 -O http://ftp.ensemblorg.ebi.ac.uk/pub/data_files/homo_sapiens/GRCh37/variation_genotype/gnomad/r2.1/${type}/${prefix}${chrom}_noVEP.vcf.gz
+  gunzip -c ${prefix}${chrom}_noVEP.vcf.gz | head -n 1200 > ${prefix}${chrom}_noVEP.vcf
   bgzip -f ${prefix}${chrom}_noVEP.vcf
   tabix ${prefix}${chrom}_noVEP.vcf.gz
 done
