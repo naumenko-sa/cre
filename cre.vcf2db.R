b312a095606e62ebacd672c09b200e4d2b6acf11
Madeline Couse <mhcouse@gmail.com>
2020-05-05 15:31:17 -0400

    modify (ref)/. genotypes to have zygosity - instead of het (CHEO-160)

--- cre.vcf2db.R
+++ cre.vcf2db.R
@@ -26,10 +26,12 @@ genotype2zygocity <- function (genotype_str, ref){
     # greedy
     genotype_str <- gsub("|", "/", genotype_str, fixed = T)
     genotype_str <- gsub("./.", "Insufficient_coverage", genotype_str, fixed = T)
-    genotype_str = gsub(".","NO_CALL",genotype_str,fixed=T)
+    genotype_str = gsub("/.","NO_CALL",genotype_str,fixed=T)
       
     if(grepl("Insufficient_coverage", genotype_str)){
       result <- genotype_str
+    }else if(grepl("NO_CALL", genotype_str)){
+      result <- '-'
     }else{
         ar <- strsplit(genotype_str, "/", fixed = T)
         len <- length(ar[[1]])
